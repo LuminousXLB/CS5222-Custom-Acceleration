50351d1c90d2301d66b2fdab523a8d1d7915d323
Shen Jiamin <shen_jiamin@comp.nus.edu.sg>
2022-03-03 22:30:55 +0800

    init hls files

---
+++ zynq/hls/mmult_float/hls_nopipe.tcl
@@ -0,0 +1,14 @@
+# Commands to compile your matrix multiply design
+set src_dir "."
+open_project accel
+set_top mmult_hw
+add_files $src_dir/mmult_float.cpp
+add_files -tb $src_dir/mmult_test.cpp
+open_solution "solution0" -flow_target vivado
+set_part {xc7z020clg484-1}
+create_clock -period 10 -name default
+config_compile -pipeline_loops 0
+csim_design -clean
+csynth_design
+close_project
+exit
