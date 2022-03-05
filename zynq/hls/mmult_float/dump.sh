854d932e10b96964295be380ffd51dc527fd8826
Shen Jiamin <shen_jiamin@comp.nus.edu.sg>
2022-03-05 16:14:50 +0800

    partition

--- zynq/hls/mmult_float/dump.sh
+++ zynq/hls/mmult_float/dump.sh
@@ -3,9 +3,11 @@
 set -x
 set -e
 
-vitis_hls -f hls_nopipe.tcl
-
 DIR=archive/$(date +%Y%m%d-%H%M%S)$1
 mkdir $DIR
+git diff mmult_float.cpp > $DIR/diff
+
+vitis_hls -f hls_nopipe.tcl
+
 mv accel/solution0/solution0.log $DIR
 cp accel/solution0/syn/report/mmult_hw_csynth.rpt $DIR
