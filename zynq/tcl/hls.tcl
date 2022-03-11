ff63c96fb54abb4814e1e558288e8a8f65fd0606
Shen Jiamin <shen_jiamin@comp.nus.edu.sg>
2022-03-11 18:51:19 +0800

    update

--- zynq/tcl/hls.tcl
+++ zynq/tcl/hls.tcl
@@ -5,11 +5,17 @@ open_project accel
 set_top mmult_hw
 add_files $src_dir/mmult_float.cpp
 add_files -tb $src_dir/mmult_test.cpp
-open_solution "solution0"
+open_solution "solution0" -flow_target vivado
 set_part {xc7z020clg484-1}
 create_clock -period 10 -name default
+
+set_clock_uncertainty 12.5%
+
+config_export -vivado_phys_opt place
+config_export -vivado_optimization_level 2
+
 csim_design -clean
 csynth_design
-export_design -evaluate verilog -format ip_catalog 
+export_design -format ip_catalog 
 close_project
 exit
