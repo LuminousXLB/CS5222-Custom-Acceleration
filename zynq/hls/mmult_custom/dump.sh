db3a6b23e471a04be68e24dad0dbd72bc85e4a7d
Shen Jiamin <shen_jiamin@comp.nus.edu.sg>
2022-03-18 15:16:36 +0800

    init hls custom

---
+++ zynq/hls/mmult_custom/dump.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+set -e
+
+DIR=../archive/$(date +%Y%m%d-%H%M%S)$1
+mkdir -p $DIR
+cp mmult*.cpp $DIR
+cp mmult.h $DIR
+
+vitis_hls -f hls.tcl
+
+mv vitis_hls.log $DIR/hls.log
+cp accel/solution0/syn/report/mmult_hw_csynth.rpt $DIR
+cp accel/solution0/solution0_data.json $DIR
